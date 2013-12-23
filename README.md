b1ba610ac92f81edd4b4383fe50a0d31c9563c03
MaxWofford <max.speed.wofford@gmail.com>
2013-12-22 18:43:51 -0800

    Set up basic structure and updated readme

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 Number Guessing Game
 ===================
 The objective of this game is to guess the number in the lowest number of turns. The player makes a guess and is told 'too low' or 'too high'. The number of tries it takes to guess the number is the player's score. 
+
+
+In Addition...
+===
+The feedback you get will be the most demotivational that I've ever made. Expect to feel bad about your life after this.
---
+++ js/script.js
@@ -0,0 +1,30 @@
+var target = Math.random();
+var score = 0;
+
+switch(score)
+{
+case 1:
+  document.getElementById("only-button").value = "I'm feeling" + "ok...";
+  break;
+case 2:
+  document.getElementById("only-button").value = "I'm feeling" + "low on self esteem...";
+  break;
+ case 3:
+  document.getElementById("only-button").value = "I'm feeling" + "";
+  break;
+ case 4:
+  document.getElementById("only-button").value = "I'm feeling" + "like this game is a reflection on my failures in life...";
+  break;
+ case 5:
+  document.getElementById("only-button").value = "I'm feeling" + "so bad at this the button can't even think of a better insult...";
+  break;
+default:
+  document.getElementById("only-button").value = "I'm feeling" + "lucky...";
+}
+
+if (document.getElementById("only-numberbox").value= target;) {
+	document.write("Is there anything else you'd like?<br>");
+}
+else {
+	document.write("Is there anything else you'd like?<br>");
+}
